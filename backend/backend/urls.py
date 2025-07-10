016f176de9e46a3eba511958fbb892e4b483157d
Jacobsmiths <jaco.smith589@gmail.com>
2025-07-10 17:42:51 -0500

    added prefix to the api

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -23,7 +23,7 @@ from ArtDatabase import urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('', include(urls))
+    path('api/', include(urls))
 ]
 
 if settings.DEBUG:
