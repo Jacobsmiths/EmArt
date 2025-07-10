14de6c18f7ad8e7163604957fc3afb640cc694b2
Jacobsmiths <jaco.smith589@gmail.com>
2025-07-10 17:49:05 -0500

    more stupid stuff

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -51,7 +51,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", default="")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
