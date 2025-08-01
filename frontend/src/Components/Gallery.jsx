7d898480b81ffd9c6053944c617e60766f271126
Jacob Smith <jaco.smith589@gmail.com>
2025-08-01 15:54:00 -0500

    removed the horizontal scrolling on the gallery view page

--- frontend/src/Components/Gallery.jsx
+++ frontend/src/Components/Gallery.jsx
@@ -5,141 +5,138 @@ import Spinner from "./Spinner";
 import { useAuth } from "../Contexts/AuthContext";
 
 const Gallery = () => {
-    const GALLERY_HEIGHT = 650;
-    const GALLERY_WIDTH = 2500;
-    const PAINTING_SCALE = 8;
-    const MINIMAP_SCALE = 15;
+  const GALLERY_HEIGHT = 650;
+  const GALLERY_WIDTH = 2500;
+  const PAINTING_SCALE = 8;
+  const MINIMAP_SCALE = 15;
 
-    const baseurl = import.meta.env.VITE_API_URL;
+  const baseurl = import.meta.env.VITE_API_URL;
 
-    const minimapHeight = Math.floor(GALLERY_HEIGHT / MINIMAP_SCALE);
-    const minimapWidth = Math.floor(GALLERY_WIDTH / MINIMAP_SCALE);
+  const minimapHeight = Math.floor(GALLERY_HEIGHT / MINIMAP_SCALE);
+  const minimapWidth = Math.floor(GALLERY_WIDTH / MINIMAP_SCALE);
 
-    const minimapRef = useRef(null);
-    const galleryViewRef = useRef(null);
-    const { token, isAuthenticated } = useAuth();
+  const minimapRef = useRef(null);
+  const galleryViewRef = useRef(null);
+  const { token, isAuthenticated } = useAuth();
 
-    const [pictureData, setPictureData] = useState({});
-    const [loading, setLoading] = useState(true);
+  const [pictureData, setPictureData] = useState({});
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const fetchPaintings = async () => {
-            try {
-                const response = await fetch(`${baseurl}/gallery-paintings/`, {
-                    method: "GET",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                });
-                const data = await response.json();
-                const paintingsById = Object.fromEntries(
-                    data.map((painting) => [painting.id, painting])
-                );
-                setPictureData(paintingsById);
-                setLoading(false);
-            } catch (err) {
-                console.error(err);
-            }
-        };
-        setLoading(true);
-        fetchPaintings();
-    }, []);
-
-    // this is because the gallery view was still scrolling up and down instead of horizontal
-    useEffect(() => {
-        if (!galleryViewRef) return;
-        galleryViewRef.current.addEventListener("wheel", (e) => {
-            e.preventDefault();
+  useEffect(() => {
+    const fetchPaintings = async () => {
+      try {
+        const response = await fetch(`${baseurl}/gallery-paintings/`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
         });
-    }, []);
-
-    const handleDrag = (x, y, id) => {
-        setPictureData((prev) => ({
-            ...prev,
-            [id]: {
-                ...prev[id],
-                Xpos: x,
-                Ypos: y,
-            },
-        }));
+        const data = await response.json();
+        const paintingsById = Object.fromEntries(
+          data.map((painting) => [painting.id, painting])
+        );
+        setPictureData(paintingsById);
+        setLoading(false);
+      } catch (err) {
+        console.error(err);
+      }
     };
+    setLoading(true);
+    fetchPaintings();
+  }, []);
 
-    const updatePositions = async () => {
-        setLoading(true);
-        try {
-            await Promise.all(
-                Object.values(pictureData).map(async (galleryPainting) => {
-                    const response = await fetch(
-                        `${baseurl}/gallery-paintings/${galleryPainting.id}/`,
-                        {
-                            method: "PUT",
-                            body: JSON.stringify({
-                                Xpos: galleryPainting.Xpos,
-                                Ypos: galleryPainting.Ypos,
-                            }),
-                            headers: {
-                                "Content-Type": "application/json",
-                                Authorization: `Token ${token}`,
-                            },
-                        }
-                    );
-                    if (!response.ok) {
-                        const errorText = await response.text();
-                        console.error(
-                            `Failed to update ${galleryPainting.id}:`,
-                            errorText
-                        );
-                    }
-                })
-            );
-        } catch (err) {
-            console.error("Unexpected error:", err);
-        } finally {
-            setLoading(false);
-        }
-    };
+  // // this is because the gallery view was still scrolling up and down instead of horizontal
+  // useEffect(() => {
+  //     if (!galleryViewRef) return;
+  //     galleryViewRef.current.addEventListener("wheel", (e) => {
+  //         e.preventDefault();
+  //     });
+  // }, []);
 
-    return (
-        <>
-            <GalleryView
-                galleryViewRef={galleryViewRef}
-                className="w-full "
-                galleryStyle={{
-                    width: `${GALLERY_WIDTH}px`,
-                    height: `${GALLERY_HEIGHT}px`,
-                }}
-                PAINTING_SCALE={PAINTING_SCALE}
-                pictureData={pictureData}
-                setPictureData={setPictureData}
-                onDrag={handleDrag}
-            />
-            {isAuthenticated && (
-                <div className="w-full flex justify-end px-12">
-                    <button
-                        onClick={() => {
-                            console.log(pictureData);
-                            updatePositions();
-                        }}
-                        className="bg-green-400 font-bold text-white p-4 rounded-lg cursor-auto"
-                    >
-                        Update Positions
-                    </button>
-                </div>
-            )}
-            <Minimap
-                PAINTING_SCALE={PAINTING_SCALE}
-                minimapRef={minimapRef}
-                galleryViewRef={galleryViewRef}
-                className="justify-center hover:opacity-80 opacity-40 transition-opacity duration-300 "
-                MINIMAP_SCALE={MINIMAP_SCALE}
-                style={{
-                    width: `${minimapWidth}px`,
-                    height: `${minimapHeight}px`,
-                }}
-                pictureData={pictureData}
-            />
-        </>
-    );
+  const handleDrag = (x, y, id) => {
+    setPictureData((prev) => ({
+      ...prev,
+      [id]: {
+        ...prev[id],
+        Xpos: x,
+        Ypos: y,
+      },
+    }));
+  };
+
+  const updatePositions = async () => {
+    setLoading(true);
+    try {
+      await Promise.all(
+        Object.values(pictureData).map(async (galleryPainting) => {
+          const response = await fetch(
+            `${baseurl}/gallery-paintings/${galleryPainting.id}/`,
+            {
+              method: "PUT",
+              body: JSON.stringify({
+                Xpos: galleryPainting.Xpos,
+                Ypos: galleryPainting.Ypos,
+              }),
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Token ${token}`,
+              },
+            }
+          );
+          if (!response.ok) {
+            const errorText = await response.text();
+            console.error(`Failed to update ${galleryPainting.id}:`, errorText);
+          }
+        })
+      );
+    } catch (err) {
+      console.error("Unexpected error:", err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <GalleryView
+        galleryViewRef={galleryViewRef}
+        className="w-full "
+        galleryStyle={{
+          width: `${GALLERY_WIDTH}px`,
+          height: `${GALLERY_HEIGHT}px`,
+        }}
+        PAINTING_SCALE={PAINTING_SCALE}
+        pictureData={pictureData}
+        setPictureData={setPictureData}
+        onDrag={handleDrag}
+      />
+      {isAuthenticated && (
+        <div className="w-full flex justify-end px-12">
+          <button
+            onClick={() => {
+              console.log(pictureData);
+              updatePositions();
+            }}
+            className="bg-green-400 font-bold text-white p-4 rounded-lg cursor-auto"
+          >
+            Update Positions
+          </button>
+        </div>
+      )}
+      <Minimap
+        PAINTING_SCALE={PAINTING_SCALE}
+        minimapRef={minimapRef}
+        galleryViewRef={galleryViewRef}
+        className="justify-center hover:opacity-80 opacity-40 transition-opacity duration-300 "
+        MINIMAP_SCALE={MINIMAP_SCALE}
+        style={{
+          width: `${minimapWidth}px`,
+          height: `${minimapHeight}px`,
+        }}
+        pictureData={pictureData}
+      />
+    </>
+  );
 };
 
 export default Gallery;
--- frontend/src/Components/GalleryView.jsx
+++ frontend/src/Components/GalleryView.jsx
@@ -4,107 +4,106 @@ import GalleryPainting from "./GalleryPainting";
 import { useAuth } from "../Contexts/AuthContext";
 
 const GalleryView = ({
-    galleryViewRef,
-    className,
-    galleryStyle,
-    pictureData,
-    PAINTING_SCALE,
-    setPictureData,
-    onDrag,
-    ...props
+  galleryViewRef,
+  className,
+  galleryStyle,
+  pictureData,
+  PAINTING_SCALE,
+  setPictureData,
+  onDrag,
+  ...props
 }) => {
-    const INCH_TO_PIXELS = 96;
-    const galleryRef = useRef(null);
-    const { isAuthenticated } = useAuth();
-    const handleScroll = (e) => {
-        if (!galleryViewRef.current) return;
-        galleryViewRef.current.scrollLeft += e.deltaY;
+  const INCH_TO_PIXELS = 96;
+  const galleryRef = useRef(null);
+  const { isAuthenticated } = useAuth();
+  const handleScroll = (e) => {
+    if (!galleryViewRef.current) return;
+    galleryViewRef.current.scrollLeft += e.deltaY;
+  };
+
+  useEffect(() => {
+    const gallery = galleryRef.current;
+    const galleryView = galleryViewRef.current;
+    if (!gallery || !galleryView) return;
+    galleryView.scrollLeft =
+      gallery.getBoundingClientRect().width / 2 -
+      galleryView.getBoundingClientRect().width / 2;
+
+    let startX = 0;
+    let scrollStart = 0;
+
+    const handleTouchStart = (e) => {
+      startX = e.touches[0].clientX;
+      scrollStart = galleryView.scrollLeft;
     };
 
-    useEffect(() => {
-        const gallery = galleryRef.current;
-        const galleryView = galleryViewRef.current;
-        if (!gallery || !galleryView) return;
-        galleryView.scrollLeft =
-            gallery.getBoundingClientRect().width / 2 -
-            galleryView.getBoundingClientRect().width / 2;
-
-        let startX = 0;
-        let scrollStart = 0;
-
-        const handleTouchStart = (e) => {
-            startX = e.touches[0].clientX;
-            scrollStart = galleryView.scrollLeft;
-        };
-
-        const handleTouchMove = (e) => {
-            const currentX = e.touches[0].clientX;
-            const delta = currentX - startX;
-            galleryView.scrollLeft = scrollStart - delta;
-        };
-
-        gallery.addEventListener("wheel", handleScroll);
-        gallery.addEventListener("touchstart", handleTouchStart);
-        gallery.addEventListener("touchmove", handleTouchMove);
-
-        return () => {
-            gallery.removeEventListener("wheel", handleScroll);
-            gallery.removeEventListener("touchstart", handleTouchStart);
-            gallery.removeEventListener("touchmove", handleTouchMove);
-        };
-    }, []);
-
-    const galleryPaintings = () => {
-        return Object.values(pictureData).map((galleryPainting) => (
-            <Draggable
-                key={`${galleryPainting.id}`}
-                id={galleryPainting.id}
-                onDrag={(x, y, id) => {
-                    onDrag(x, y, id);
-                }}
-                startingPos={{
-                    x: galleryPainting.Xpos,
-                    y: galleryPainting.Ypos,
-                }}
-                draggable={isAuthenticated}
-            >
-                <GalleryPainting
-                    painting={galleryPainting.painting}
-                    container={galleryRef}
-                    style={{
-                        height: `${
-                            (galleryPainting.painting.height * INCH_TO_PIXELS) /
-                            PAINTING_SCALE
-                        }px`,
-                        width: `${
-                            (galleryPainting.painting.width * INCH_TO_PIXELS) /
-                            PAINTING_SCALE
-                        }px`,
-                    }}
-                />
-            </Draggable>
-        ));
+    const handleTouchMove = (e) => {
+      const currentX = e.touches[0].clientX;
+      const delta = currentX - startX;
+      galleryView.scrollLeft = scrollStart - delta;
     };
 
-    return (
-        <div
-            className={`relative overflow-auto z-10  ${className}`}
-            ref={galleryViewRef}
-            {...props}
-            style={{
-                scrollbarWidth: "thin",
-                scrollbarColor: "#888 #f3f3f3", // [scrollbar-width:none]
-            }}
-        >
-            <div
-                className="relative overflow-hidden z-10 "
-                ref={galleryRef}
-                style={galleryStyle}
-            >
-                {galleryPaintings()}
-            </div>
-        </div>
-    );
+    // gallery.addEventListener("wheel", handleScroll);
+    gallery.addEventListener("touchstart", handleTouchStart);
+    gallery.addEventListener("touchmove", handleTouchMove);
+
+    return () => {
+    //   gallery.removeEventListener("wheel", handleScroll);
+      gallery.removeEventListener("touchstart", handleTouchStart);
+      gallery.removeEventListener("touchmove", handleTouchMove);
+    };
+  }, []);
+
+  const galleryPaintings = () => {
+    return Object.values(pictureData).map((galleryPainting) => (
+      <Draggable
+        key={`${galleryPainting.id}`}
+        id={galleryPainting.id}
+        onDrag={(x, y, id) => {
+          onDrag(x, y, id);
+        }}
+        startingPos={{
+          x: galleryPainting.Xpos,
+          y: galleryPainting.Ypos,
+        }}
+        draggable={isAuthenticated}
+      >
+        <GalleryPainting
+          painting={galleryPainting.painting}
+          container={galleryRef}
+          style={{
+            height: `${
+              (galleryPainting.painting.height * INCH_TO_PIXELS) /
+              PAINTING_SCALE
+            }px`,
+            width: `${
+              (galleryPainting.painting.width * INCH_TO_PIXELS) / PAINTING_SCALE
+            }px`,
+          }}
+        />
+      </Draggable>
+    ));
+  };
+
+  return (
+    <div
+      className={`relative overflow-auto z-10  ${className}`}
+      ref={galleryViewRef}
+      {...props}
+      style={{
+        scrollbarWidth: "thin",
+        scrollbarColor: "#888 #f3f3f3", // [scrollbar-width:none]
+      }}
+    >
+      <div
+        className="relative overflow-hidden z-10 "
+        ref={galleryRef}
+        style={galleryStyle}
+      >
+        {galleryPaintings()}
+      </div>
+    </div>
+  );
 };
 
 export default GalleryView;
