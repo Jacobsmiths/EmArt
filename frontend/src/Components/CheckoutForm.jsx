03ec8e635748554ab07baba8907561065f1f1501
Jacob Smith <jaco.smith589@gmail.com>
2025-08-13 10:55:29 -0500

    trying to make checkout page resizeable

--- frontend/src/Components/CheckoutForm.jsx
+++ frontend/src/Components/CheckoutForm.jsx
@@ -1,60 +1,63 @@
 import React, { useCallback, useState, useEffect, useMemo } from "react";
 import {
-    EmbeddedCheckoutProvider,
-    EmbeddedCheckout,
+  EmbeddedCheckoutProvider,
+  EmbeddedCheckout,
 } from "@stripe/react-stripe-js";
 import { useCart } from "../Contexts/CartContext";
 
 const CheckoutForm = ({ stripePromise }) => {
-    const { cart } = useCart();
-    const baseurl = import.meta.env.VITE_API_URL;
+  const { cart } = useCart();
+  const baseurl = import.meta.env.VITE_API_URL;
 
-    const fetchClientSecret = useCallback(() => {
-        console.log("cart changed");
-        const cartItemIds = cart.map((item) => item.id);
-        console.log(JSON.stringify({ painting_ids: cartItemIds }));
-        return fetch(`${baseurl}/create-checkout/`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ painting_ids: cartItemIds }),
-        })
-            .then((res) => res.json())
-            .then((data) => data.clientSecret);
-    }, [cart]);
+  const fetchClientSecret = useCallback(() => {
+    console.log("cart changed");
+    const cartItemIds = cart.map((item) => item.id);
+    console.log(JSON.stringify({ painting_ids: cartItemIds }));
+    return fetch(`${baseurl}/create-checkout/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ painting_ids: cartItemIds }),
+    })
+      .then((res) => res.json())
+      .then((data) => data.clientSecret);
+  }, [cart]);
 
-    // const onShippingDetailsChange = async (shippingDetailsChangeEvent) => {
-    //     const { checkoutSessionId, shippingDetails } =
-    //         shippingDetailsChangeEvent;
-    //     console.log(shippingDetails);
-    //     const response = await fetch(`${baseurl}/validate-shipping/`, {
-    //         method: "POST",
-    //         body: JSON.stringify({
-    //             checkout_session_id: checkoutSessionId,
-    //             shipping_details: shippingDetails,
-    //         }),
-    //     });
+  // This code was an attempt at validating pickup locations
+  // this didnt work because it asked you location before you did shipping type or something
 
-    //     if (response.type === "error") {
-    //         return Promise.resolve({
-    //             type: "reject",
-    //             errorMessage: response.message,
-    //         });
-    //     } else {
-    //         return Promise.resolve({ type: "accept" });
-    //     }
-    // };
+  // const onShippingDetailsChange = async (shippingDetailsChangeEvent) => {
+  //     const { checkoutSessionId, shippingDetails } =
+  //         shippingDetailsChangeEvent;
+  //     console.log(shippingDetails);
+  //     const response = await fetch(`${baseurl}/validate-shipping/`, {
+  //         method: "POST",
+  //         body: JSON.stringify({
+  //             checkout_session_id: checkoutSessionId,
+  //             shipping_details: shippingDetails,
+  //         }),
+  //     });
 
-    const options = { fetchClientSecret };
+  //     if (response.type === "error") {
+  //         return Promise.resolve({
+  //             type: "reject",
+  //             errorMessage: response.message,
+  //         });
+  //     } else {
+  //         return Promise.resolve({ type: "accept" });
+  //     }
+  // };
 
-    return (
-        <div id="checkout" className="w-full block">
-            <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
-                <EmbeddedCheckout />
-            </EmbeddedCheckoutProvider>
-        </div>
-    );
+  const options = { fetchClientSecret };
+
+  return (
+    <div id="checkout" className="w-full block">
+      <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+        <EmbeddedCheckout />
+      </EmbeddedCheckoutProvider>
+    </div>
+  );
 };
 
 export default CheckoutForm;
--- frontend/src/Pages/CheckoutPage.jsx
+++ frontend/src/Pages/CheckoutPage.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import CheckoutForm from "../Components/CheckoutForm";
 import CartSidebar from "../Components/CartSidebar";
 import { CartList } from "../Components/CartList";
+import ResizeablePane from "../Components/ResizeablePane";
 
 const CheckoutPage = ({ stripePromise }) => {
-    return (
-        <div className="grid grid-cols-2 md:grid-cols-[1fr_1fr] h-full">
-            <title>Emersons Art | Checkout</title>
-            <CheckoutForm stripePromise={stripePromise} />
-            <CartSidebar />
-        </div>
-    );
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-[1fr_1fr] h-full">
+      <title>Emersons Art | Checkout</title>
+      <ResizeablePane>
+        <CheckoutForm stripePromise={stripePromise} />
+      </ResizeablePane>
+      <CartSidebar />
+    </div>
+  );
 };
 
 export default CheckoutPage;
